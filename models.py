8908b60d9915d23e9ecddff3eb27a00d95c305c0
Mikola000W <113891277+Mikola000W@users.noreply.github.com>
2022-10-03 10:21:23 +0200

    Add files via upload

--- models.py
+++ models.py
@@ -6,13 +6,6 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=200)
-
